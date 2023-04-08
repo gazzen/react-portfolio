bd3aa8bfd8b193c0b3525b1494d1ef8991094ec5
gazzen <gazensuwal@gmail.com>
2023-04-08 14:17:02 -0400

    commit

---
+++ src/content_option.js
@@ -0,0 +1,184 @@
+const logotext = "GAJENDRA SUWAL";
+const meta = {
+    title: "Gajendra Suwal",
+    description: "I’m Gajendra Suwal data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title:
+    "I’m Gajendra Suwal, like to work as full stack React JavaScript Web Devloper",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I am full stack Javascript Developer",
+  },
+  description:
+    "I am passionate about creating beautiful, intuitive front-end frameworks and  back-end to create engaging and responsive web appplications. And always looking for ways to improve my skills , stay up to date with emerging technologies.",
+  your_img_url:
+    "https://pbs.twimg.com/profile_banners/1355241577531240451/1638263481/1500x500",
+};
+
+const dataabout = {
+  title: "A bit about my self",
+  aboutme:
+    "I am passionate about creating beautiful, intuitive front-end frameworks and  back-end to create engaging and responsive web appplications. And always looking for ways to improve my skills , stay up to date with emerging technologies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "Google Developer",
+        date: "2023",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "xyz company",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 40,
+    },
+    {
+        name: "Djano",
+        value: 55,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [
+  {
+    img: "http://openaigpt.gazensuwal.com/static/media/possibility.525f13b61a2157d44de0.png",
+    description:
+      "This is the single page web design; converting UX Design into web page.",
+    link: "http://openaigpt.gazensuwal.com/",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://restaurantcafe.gazensuwal.com/",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "gajendr.suwal123@gmail.com",
+  YOUR_FONE: "(437)-972-0440",
+  description:
+    "I am passionate about creating beautiful, intuitive front-end frameworks and  back-end to create engaging and responsive web appplications. And always looking for ways to improve my skills , stay up to date with emerging technologies. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/gazzen",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/gazusuwal",
+    twitter: "https://twitter.com/@suwalgajendra",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
